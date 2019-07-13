b7657dc4ac4792c653e721abfe611085cd47e12f
29781 <2978183194@qq.com>
2019-07-13 19:14:21 +0800

    搜索中心部分提交

---
+++ xiaomall-search/src/main/java/com/xiao/xiaomall/search/XiaomallSearchApplication.java
@@ -0,0 +1,13 @@
+package com.xiao.xiaomall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaomallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaomallSearchApplication.class, args);
+    }
+
+}
